b94779606a48f36d17922547ac631f24d6303e62
geramirezl <70707336+geramirezl@users.noreply.github.com>
2020-11-07 15:07:40 -0500

    Taller Persistencia en BD realizado

---
+++ LoginApplication/build/generated-sources/ap-source-output/Entidad/Usuario_.java
@@ -0,0 +1,15 @@
+package Entidad;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-07T14:27:42")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> password;
+    public static volatile SingularAttribute<Usuario, Integer> id;
+    public static volatile SingularAttribute<Usuario, String> nombre;
+
+}
